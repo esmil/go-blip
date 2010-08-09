8189dba8ea4d86dedd63d03df44d53eafd929669
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-08-09 19:28:00 +0200

    Make blip use the new termios interface code.

--- blip.go
+++ blip.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"container/list"
+	"os"
+	"termios"
 	"time"
-	"serial"
 )
 
 const (
@@ -16,13 +18,15 @@ type blip int64
 func spawnFetcher() chan blip {
 	c := make(chan blip, inFlightMessages)
 	go func() {
-		sp, err := serial.Open()
+		sp, err := termios.Open("/dev/tty.usbserial", os.O_RDONLY, 0)
 		if err != nil {
 			panic(err)
 		}
+		//sp.Set(termios.B9600)
 
+		b := make([]byte, 1)
 		for {
-			sp.ReadLine()
+			sp.Read(b)
 			c <- blip(time.Nanoseconds())
 		}
 	}()
@@ -30,16 +34,23 @@ func spawnFetcher() chan blip {
 	return c
 }
 
+func storeInDb(l *list.List) {
+	return
+}
+
 func main() {
 	fmt.Printf("Blip storage daemon %s\n", version)
 	fetchC := spawnFetcher()
+	l := list.New()
 
 	for {
 		select {
 		case x := <-fetchC:
+			l.PushBack(x)
 			fmt.Printf("Read blip %s", x)
 		default:
-			fmt.Printf("Nothing to do")
+			storeInDb(l)
+			time.Sleep(60 * 1000000000)
 		}
 	}
 }
